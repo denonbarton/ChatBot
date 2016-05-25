1bf3eaeb7be7ff3ccc5da42e653a889c3b5ac933
denonbarton <dbar0540@740s-pl-126.local>
2016-05-25 14:06:23 -0600

    updated reading

--- src/chat/controller/IOController.java
+++ src/chat/controller/IOController.java
@@ -10,19 +10,32 @@ import javax.swing.JOptionPane;
 
 public class IOController
 {
-
+	/**
+	 * Saves the supplied text to a file with the current system time as a part
+	 * of the file name
+	 * @param textToSave The supplied text.
+	 * @return The name of the file.
+	 */
 	
-	public static String saveFile(String TextToSave)
+	
+	public static String saveFile(String textToSave)
 	{
-		String fileName = "Saved Chat File - ";
-		
+		String fileName = "Saved chat File -";
+		fileName += Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+		fileName += ":" + Calendar.getInstance().get(Calendar.MINUTE);
+		fileName += ".txt";
+		FileWriter chatWriter;
 		try
 		{
-	
+			chatWriter = new FileWriter(fileName);
+			chatWriter.write("this is the " + fileName + "\n");
+			chatWriter.write(textToSave);
+			chatWriter.close();
+			JOptionPane.showMessageDialog(null,  "File saved as: " + fileName);
 		}
 		catch (IOException ioError)
 		{
-			JOptionPane.showMessageDialog(null, ioError.getMessage());			
+			JOptionPane.showMessageDialog(null, ioError.getMessage());
 		}
 		return fileName;
 	}
@@ -37,8 +50,16 @@ public class IOController
 			chatScanner = new Scanner (chatTextFile);
 			while(chatScanner.hasNext())
 			{
-				
+				text += chatScanner.nextLine();
 			}
+			chatScanner.close();
+			JOptionPane.showMessageDialog(null,  fileName + " was loaded to the chatArea");
+		
 		}
+		catch (IOException ioError)
+		{
+			JOptionPane.showMessageDialog(null, fileName + " was loaded to the chatArea");
+		}
+		return text;
 	}
 }
